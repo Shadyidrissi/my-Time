bb2c8e5c4e7acc45d3cd54798889f6f3a08b32c1
Chadiydrissi <onstockbego@gmail.com>
2025-04-08 20:10:57 +0100

    start project

---
+++ admin/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div id='navbar'>
+        <a href="">hello</a>
+        <a href="">hello</a>
+        <a href="">hello</a>
+    </div>
+  )
+}
+
+export default Navbar
